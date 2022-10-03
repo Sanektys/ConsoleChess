4e9a33a17551fa739574e5f458dc7b87481471c7
Ilya Shesterov <Sanektisru@gmail.com>
2022-10-04 02:00:23 +0700

    Updated method "canMoveToPosition" in class "Pawn"
    Now this method prevent motion if any chess piece is exist ahead, and allow attack
    enemy in diagonal step

--- src/Chess/Pawn.java
+++ src/Chess/Pawn.java
@@ -20,22 +20,29 @@ public class Pawn extends ChessPiece {
         if (!(chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn))) {
             return false;
         }
-        if (column != toColumn) {
-            return false;
-        }
         if (color.equals("White")) {
             int step = toLine - line;
+            if (column != toColumn) {
+                int attackStep = Math.abs(column - toColumn);
+                return step == 1 && attackStep == 1 && chessBoard.board[toLine][toColumn] != null &&
+                        !chessBoard.board[toLine][toColumn].getColor().equals(this.color);
+            }
             if (line == 1) {
-                return step == 1 || step == 2;
+                return (step == 1 || step == 2) && chessBoard.board[toLine][column] == null;
             } else {
-                return step == 1;
+                return step == 1 && chessBoard.board[toLine][column] == null;
             }
         } else if (color.equals("Black")) {
             int step = line - toLine;
+            if (column != toColumn) {
+                int attackStep = Math.abs(column - toColumn);
+                return step == 1 && attackStep == 1 && chessBoard.board[toLine][toColumn] != null &&
+                        !chessBoard.board[toLine][toColumn].getColor().equals(this.color);
+            }
             if (line == 6) {
-                return step == 1 || step == 2;
+                return (step == 1 || step == 2) && chessBoard.board[toLine][column] == null;
             } else {
-                return step == 1;
+                return step == 1 && chessBoard.board[toLine][column] == null;
             }
         }
         return false;
