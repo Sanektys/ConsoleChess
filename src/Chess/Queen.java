47fa22c9c0747e0d7cc894644dee2df7185233de
Ilya Shesterov <Sanektisru@gmail.com>
2022-10-04 00:59:36 +0700

    Update method "canMoveToPosition" in class "Queen"
    Now this method can check intersection between chess pieces during a move

--- src/Chess/Queen.java
+++ src/Chess/Queen.java
@@ -23,9 +23,32 @@ public class Queen extends ChessPiece {
         boolean isGoingVertical   = Math.abs(line - toLine) > 0;
         boolean isGoingHorizontal = Math.abs(column - toColumn) > 0;
         if ((isGoingVertical && !isGoingHorizontal) || (!isGoingVertical && isGoingHorizontal)) {
+            if (isGoingVertical) {
+                for (int i = Math.min(line, toLine) + 1; i < Math.max(line, toLine); i++) {
+                    if (chessBoard.board[i][column] != null) {
+                        return false;
+                    }
+                }
+            } else {
+                for (int i = Math.min(column, toColumn) + 1; i < Math.max(column, toColumn); i++) {
+                    if (chessBoard.board[line][i] != null) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else if (Math.abs(line - toLine) == Math.abs(column - toColumn)) {
+            int deltaLine   = (toLine > line) ? 1 : -1;
+            int deltaColumn = (toColumn > column) ? 1 : -1;
+            for (int i = line + deltaLine, j = column + deltaColumn; i != toLine;
+                 i += deltaLine, j += deltaColumn) {
+                if (chessBoard.board[i][j] != null) {
+                    return false;
+                }
+            }
             return true;
         } else {
-            return Math.abs(line - toLine) == Math.abs(column - toColumn);
+            return false;
         }
     }
 
